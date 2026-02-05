20269294fbb09c8325be8dd3d84777fac944f514
christophrunkel <32742371+christophrunkel@users.noreply.github.com>
2026-02-05 13:07:21 +0100

    Update ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array (
   array (
     'depends' => 
     array (
-      'typo3' => '11.5.0-12.5.99',
+      'typo3' => '12.4.0-13.4.99',
     ),
     'conflicts' => 
     array (
